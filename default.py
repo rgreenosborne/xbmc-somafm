400e5adc8fa5aeabd32eb2003b03bbe1efb230cd
Oderik <oderikmail-github@yahoo.de>
2014-10-01 19:45:22 +0200

    Apply IDEA code style Rename variable stations to station as it represents one single station at a time Use best quality playlist for now Playback first entry of station playlist instead of entire playlist to avoid the corresponding sub menu (Many changes because I started without version control)

--- default.py
+++ default.py
@@ -1,22 +1,30 @@
-import os,sys,urllib2
-import xbmcplugin,xbmcgui
+import sys
+import urllib2
 import xml.etree.ElementTree as ET
+
+import xbmcplugin
+import xbmcgui
+import xbmc
+
 
 __addon__ = "SomaFM"
 __addonid__ = "plugin.audio.somafm"
 __version__ = "0.0.2"
 
+
 def log(msg):
     print "[PLUGIN] '%s (%s)' " % (__addon__, __version__) + str(msg)
+
 
 log("Initialized!")
 log(sys.argv)
 
 rootURL = "http://somafm.com/"
 
-#pluginPath = sys.argv[0]
+# pluginPath = sys.argv[0]
 handle = int(sys.argv[1])
 query = sys.argv[2]
+
 
 def getHeaders(withReferrer=None):
     headers = {}
@@ -24,6 +32,7 @@ def getHeaders(withReferrer=None):
     if withReferrer:
         headers['Referrer'] = withReferrer
     return headers
+
 
 def getHTMLFor(url, withData=None, withReferrer=None):
     url = rootURL + url
@@ -34,18 +43,39 @@ def getHTMLFor(url, withData=None, withReferrer=None):
     response.close()
     return data
 
+
+def get_best_playlist(station):
+    for playlist_key in ['highestpls', 'fastpls', 'slowpls']:
+        playlist = station.find(playlist_key)
+        if playlist is not None:
+            log('Using {} playlist {}'.format(playlist_key, playlist.text))
+            return playlist
+
+
+def get_content_url(station):
+    url = rootURL + get_best_playlist(station).text.replace(rootURL, "")
+    play_list = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
+    play_list.load(url)
+    item = play_list.__getitem__(0)
+    return item.getfilename()
+
+
 def addEntries():
     somaXML = getHTMLFor(url="channels.xml")
     channelsContainer = ET.fromstring(somaXML)
 
-    for stations in channelsContainer.findall(".//channel"):
-        title = stations.find('title').text
-        description = stations.find('description').text
-        if stations.find('largeimage') is not None:
-            img = rootURL + stations.find('largeimage').text.replace(rootURL,"")
+    for station in channelsContainer.findall(".//channel"):
+        title = station.find('title').text
+        description = station.find('description').text
+        # if station.find('listeners') is not None:
+        #     title = '%(description)s (%(listeners)s listeners)' \
+        #                   % {"description": description,
+        #                      "listeners": station.find('listeners').text}
+        if station.find('largeimage') is not None:
+            img = rootURL + station.find('largeimage').text.replace(rootURL, "")
         else:
-            img = rootURL + stations.find('image').text.replace(rootURL,"")
-        url = rootURL + stations.find('fastpls').text.replace(rootURL,"")
+            img = rootURL + station.find('image').text.replace(rootURL,"")
+        url = get_content_url(station)
         log(title)
         log(description)
         log(img)
@@ -56,6 +86,7 @@ def addEntries():
             handle=handle,
             url=url,
             listitem=li)
+
 
 addEntries()
 xbmcplugin.endOfDirectory(handle)
